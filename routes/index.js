29b505e2e6b84b3da7ef9a7aa249a41ce7d7b9a8
Rob Conery <rob@wekeroad.com>
2013-07-19 14:49:59 +0800

    A bit more prep

--- routes/index.js
+++ routes/index.js
@@ -9,46 +9,46 @@ var Routes = function(app){
     res.render("list");
   });
 
-  app.get("angular/simple", function(req,res){
-    res.render("angular/simple");
+  app.get("/angular/simple", function(req,res){
+    res.render("angular_simple");
   });
-  app.get("angular/list", function(req,res){
-    res.render("angular/list");
+  app.get("/angular/list", function(req,res){
+    res.render("angular_list");
   });
-  app.get("angular/save", function(req,res){
-    res.render("angular/save");
-  });
-
-
-  app.get("backbone/simple", function(req,res){
-    res.render("backbone/simple");
-  });
-  app.get("backbone/list", function(req,res){
-    res.render("backbone/list");
-  });
-  app.get("backbone/save", function(req,res){
-    res.render("backbone/save");
+  app.get("/angular/save", function(req,res){
+    res.render("angular_save");
   });
 
 
-  app.get("knockout/simple", function(req,res){
-    res.render("knockout/simple");
+  app.get("/backbone/simple", function(req,res){
+    res.render("backbone_simple");
   });
-  app.get("knockout/list", function(req,res){
-    res.render("knockout/list");
+  app.get("/backbone/list", function(req,res){
+    res.render("backbone_list");
   });
-  app.get("knockout/save", function(req,res){
-    res.render("knockout/save");
+  app.get("/backbone/save", function(req,res){
+    res.render("backbone_save");
   });
 
-  app.get("ember/simple", function(req,res){
-    res.render("ember/simple");
+
+  app.get("/knockout/simple", function(req,res){
+    res.render("ko_simple");
   });
-  app.get("ember/list", function(req,res){
-    res.render("ember/list");
+  app.get("/knockout/list", function(req,res){
+    res.render("ko_list");
   });
-  app.get("ember/save", function(req,res){
-    res.render("ember/save");
+  app.get("/knockout/save", function(req,res){
+    res.render("ko_save");
+  });
+
+  app.get("/ember/simple", function(req,res){
+    res.render("ember_simple");
+  });
+  app.get("/ember/list", function(req,res){
+    res.render("ember_list");
+  });
+  app.get("/ember/save", function(req,res){
+    res.render("ember_save");
   });
 };
 
