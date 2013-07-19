db1b2d6f727ef6ac18c8c8a741f724b1954a2fc3
Rob Conery <rob@wekeroad.com>
2013-07-19 14:18:44 +0800

    Seeded for all things

--- app.js
+++ app.js
@@ -4,8 +4,6 @@
  */
 
 var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
   , path = require('path');
 
@@ -27,11 +25,12 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+//to save us from Backbone
+var ejs = require('ejs');
+ejs.open = '[[';
+ejs.close = ']]';
 
-var store = require("./lib/store")(app);
-
+var routes = require("./routes")(app);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,58 @@
 
-/*
- * GET home page.
- */
+var Routes = function(app){
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  app.get("/", function(req, res){
+    res.render('index');
+  });
+
+  app.get("/list", function(req,res){
+    res.render("list");
+  });
+
+  app.get("angular/simple", function(req,res){
+    res.render("angular/simple");
+  });
+  app.get("angular/list", function(req,res){
+    res.render("angular/list");
+  });
+  app.get("angular/save", function(req,res){
+    res.render("angular/save");
+  });
+
+
+  app.get("backbone/simple", function(req,res){
+    res.render("backbone/simple");
+  });
+  app.get("backbone/list", function(req,res){
+    res.render("backbone/list");
+  });
+  app.get("backbone/save", function(req,res){
+    res.render("backbone/save");
+  });
+
+
+  app.get("knockout/simple", function(req,res){
+    res.render("knockout/simple");
+  });
+  app.get("knockout/list", function(req,res){
+    res.render("knockout/list");
+  });
+  app.get("knockout/save", function(req,res){
+    res.render("knockout/save");
+  });
+
+  app.get("ember/simple", function(req,res){
+    res.render("ember/simple");
+  });
+  app.get("ember/list", function(req,res){
+    res.render("ember/list");
+  });
+  app.get("ember/save", function(req,res){
+    res.render("ember/save");
+  });
 };
+
+
+
+module.exports = Routes;
+
